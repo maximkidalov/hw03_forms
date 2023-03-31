c295de6b76b0d5687dcd16d6f8813df47cbdd4e9
maximkidalov <117586406+maximkidalov@users.noreply.github.com>
2023-03-31 14:17:35 +0300

    Add files via upload

---
+++ yatube/posts/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['text', 'group']
+        help_texts = {
+            'text': 'Текст нового поста',
+            'group': 'Группа, к которой будет относиться пост',
+        }
+        verbose_name = ('Текст нового поста')
+        verbose_name_plural = ('Текст новых постов')
